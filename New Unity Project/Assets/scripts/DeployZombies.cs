93950ba417182710c0be840e61784816a77947cd
Bernard Strachan II <bernardstrachan@gmail.com>
2022-05-04 19:55:28 -0400

    Zombie Spawning
    The zombies spawn in different places at different speeds

---
+++ New Unity Project/Assets/scripts/DeployZombies.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeployZombies : MonoBehaviour
+{
+    public GameObject [] zombiePrefab;
+    public float respawnTime = 1.0f;
+    public float YValue = 1f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(zombieWave());
+        
+    }
+        
+
+      // Update is called once per frame
+    private void spawnEnemy() 
+    {
+        GameObject a = Instantiate(zombiePrefab[(Random.Range(0, zombiePrefab.Length))]) as GameObject;
+        a.transform.position = new Vector3(Random.Range(-5.0f, 10.0f), YValue, Random.Range(-5.0f, 10.0f));
+        Debug.Log("Spawn");
+    }
+    IEnumerator zombieWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            spawnEnemy();
+        }
+    }
+}
---
+++ New Unity Project/Assets/scripts/MovingZombie.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MovingZombie : MonoBehaviour
+{
+
+    [SerializeField] float distanceToCover;
+    [SerializeField] float speed = Random.Range(1,10);
+
+    private Vector3 startingPosition;
+
+     void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    void Update()
+    {
+
+        Vector3 v = startingPosition;
+        v.z += distanceToCover * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
