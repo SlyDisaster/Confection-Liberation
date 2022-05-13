bfe811816bd913385194a035a5530469f05b5733
Bernard Strachan II <bernardstrachan@gmail.com>
2022-05-13 19:27:48 -0400

    Zombie Heal and Scoreboard
    Zombies turn to human and added to the score.

---
+++ New Unity Project/Assets/scripts/ZombieHealed.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ZombieHealed : MonoBehaviour
+
+
+{
+    public Sprite Human;
+    public SpriteRenderer spriteRenderer;
+    public GameObject scoreBoard;
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Candy")
+        {
+            spriteRenderer.sprite = Human;
+
+            scoreBoard.GetComponent<score>().gameScore++;
+
+        }
+
+        void Start()
+        {
+            scoreBoard = GameObject.Find("ScoreBoard");
+
+        }
+
+
+    }
+}
+
+
+
+
+    // Update is called once per frame
+   
