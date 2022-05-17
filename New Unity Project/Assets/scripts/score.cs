abc79a179ff8bcf46d1c44f0cda376a497b72ef4
Bernard Strachan II <bernardstrachan@gmail.com>
2022-05-17 13:32:41 -0400

    Scoreboard and Win Condition
    Showing how many zombies to heal and when you reach 100, you win.

---
+++ New Unity Project/Assets/scripts/score.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour
+{
+    public int gameScore;
+    public Text winText;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = gameScore.ToString();
+
+        //check IF gameScore is = to 100
+
+        if(gameScore == 100)
+        {
+            YouWin();
+           
+        }
+    }
+
+    void YouWin()
+    {
+        //turn text on that says you win
+        winText.enabled = true;
+    }
+}
